873ffccd7b94ac7c5e35400e332f6aaf3e7bf114
WooSeong Chung <jungyh0528@gmail.com>
2024-07-08 00:31:17 +0900

    feat: implement persistence for blockchains

--- blockchain.go
+++ blockchain.go
@@ -1,15 +1,92 @@
 package main
 
+import (
+	"log"
+
+	"github.com/boltdb/bolt"
+)
+
+const (
+	dbFile       = "./database.db"
+	blocksBucket = "blocks"
+)
+
 type BlockChain struct {
-	Blocks []*Block
+	tip []byte
+	db  *bolt.DB
 }
 
 func NewBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{NewGenesisBlock()}}
+	var tip []byte
+
+	db, err := bolt.Open(dbFile, 0600, nil)
+	if err != nil {
+		log.Fatalln("Failed to open blockchain db: ", err)
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+
+		if b == nil {
+			genesis := NewGenesisBlock()
+			b, err := tx.CreateBucket([]byte(blocksBucket))
+			if err != nil {
+				return err
+			}
+			err = b.Put(genesis.Hash, genesis.Serialize())
+			if err != nil {
+				return err
+			}
+			err = b.Put([]byte("l"), genesis.Hash)
+			if err != nil {
+				return err
+			}
+			tip = genesis.Hash
+		} else {
+			tip = b.Get([]byte("l"))
+		}
+
+		return nil
+	})
+	if err != nil {
+		log.Fatalln("Failed to init blockchain from db: ", err)
+	}
+
+	bc := BlockChain{tip, db}
+	return &bc
 }
 
 func (bc *BlockChain) AddBlock(data string) {
-	prevBlock := bc.Blocks[len(bc.Blocks)-1]
-	newBlock := NewBlock(data, prevBlock.Hash)
-	bc.Blocks = append(bc.Blocks, newBlock)
+	var lastHash []byte
+
+	err := bc.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		lastHash = b.Get([]byte("l"))
+
+		return nil
+	})
+	if err != nil {
+		log.Fatalln("Failed to read lastHash in database: ", err)
+	}
+
+	newBlock := NewBlock(data, lastHash)
+
+	err = bc.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		err = b.Put(newBlock.Hash, newBlock.Serialize())
+		if err != nil {
+			return err
+		}
+		err = b.Put([]byte("l"), newBlock.Hash)
+		if err != nil {
+			return err
+		}
+
+		bc.tip = newBlock.Hash
+
+		return nil
+	})
+	if err != nil {
+		log.Fatalln("Failed to add NewBlock in database: ", err)
+	}
 }
